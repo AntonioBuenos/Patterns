f6069f2b2e485b3b199984d190a8ca663f6834c2
AntonioBuenos <104083531+AntonioBuenos@users.noreply.github.com>
2022-11-06 22:23:10 +0300

    Composite

---
+++ src/main/java/pattern8_composite/Runner.java
@@ -0,0 +1,33 @@
+package pattern8_composite;
+
+import pattern8_composite.editor.ImageEditor;
+import pattern8_composite.shapes.Circle;
+import pattern8_composite.shapes.CompoundShape;
+import pattern8_composite.shapes.Dot;
+import pattern8_composite.shapes.Rectangle;
+
+import java.awt.*;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        ImageEditor editor = new ImageEditor();
+
+        editor.loadShapes(
+                new Circle(10, 10, 10, Color.BLUE),
+
+                new CompoundShape(
+                        new Circle(110, 110, 50, Color.RED),
+                        new Dot(160, 160, Color.RED)
+                ),
+
+                new CompoundShape(
+                        new Rectangle(250, 250, 100, 100, Color.GREEN),
+                        new Dot(240, 240, Color.GREEN),
+                        new Dot(240, 360, Color.GREEN),
+                        new Dot(360, 360, Color.GREEN),
+                        new Dot(360, 240, Color.GREEN)
+                )
+        );
+    }
+}
