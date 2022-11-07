4a6c1ec742a58e1fb698e0d0fe93e762e787461d
AntonioBuenos <104083531+AntonioBuenos@users.noreply.github.com>
2022-11-07 17:25:10 +0300

    Composite

--- src/main/java/pattern8_composite/shapes/CompoundShape.java
+++ src/main/java/pattern8_composite/shapes/CompoundShape.java
@@ -3,6 +3,7 @@ package pattern8_composite.shapes;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class CompoundShape extends BaseShape {
     protected List<Shape> children = new ArrayList<>();
